fd0dbb7e277e20b5e74a86bfee746cfbe8d2f369
LorenzoHiver <lorenzohiver2@gmail.com>
2020-04-21 05:35:57 +0200

    🐛 modification d'un nom de variabl

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,13 @@ import { container } from '../styles/container'
 
 
 export default () => {
-  const [test, setTest] = useState(false)
+  const [hidden, setHidden] = useState(false)
 
   return (
     <>
       <Seo title="Home" />
       <div className={container}>
-        <button className="fixed left-1/2" type="button" onClick={() => setTest(value => !value)}>test</button>
+        <button className="fixed left-1/2" type="button" onClick={() => setHidden(value => !value)}>test</button>
         <Informations hidden={test} />
       </div>
     </>

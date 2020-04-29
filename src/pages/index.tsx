81f7fb7dca4d3939693e14ade2ea499a5db12bf6
LorenzoHiver <lorenzohiver2@gmail.com>
2020-04-30 01:12:41 +0200

    ✨ Mise en place de la map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { Seo } from '../components/Seo/index'
 import { Informations } from '../components/Informations/index'
 import { container } from '../styles/container'
+import { MapContainer } from '../components/MapContainer'
 
 
 export default () => {
@@ -12,7 +13,8 @@ export default () => {
     <>
       <Seo title="Home" />
       <div className={container}>
-        <button className="fixed left-1/2" type="button" onClick={() => setHidden(value => !value)}>test</button>
+        <button className="fixed left-1/2 z-20" type="button" onClick={() => setHidden(value => !value)}>test</button>
+        <MapContainer />
         <Informations hidden={hidden} />
       </div>
     </>
